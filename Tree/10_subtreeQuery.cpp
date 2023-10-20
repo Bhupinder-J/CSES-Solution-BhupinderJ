50d169ee966e162f1b2ab161b87d17cac90a6053
BhupinderJ <sbhupinderj9721@gmail.com>
2023-10-20 22:45:37 +0530

    Euler Tour

--- Tree/10_subtreeQuery.cpp
+++ Tree/10_subtreeQuery.cpp
@@ -7,41 +7,62 @@ using namespace std;
 #define pii pair<int, int>
 #define vvi vector<vector<int>>
 
-class SegmentTree {
+class SegmentTree
+{
 private:
     vector<int> segmentTree;
     int n;
-    void buildTree(const vector<int>& arr, int low, int high, int pos) {
-        if (low == high) {segmentTree[pos] = arr[low];return;}
+    void buildTree(const vector<int> &arr, int low, int high, int pos)
+    {
+        if (low == high)
+        {
+            segmentTree[pos] = arr[low];
+            return;
+        }
         int mid = (low + high) / 2;
         buildTree(arr, low, mid, 2 * pos + 1);
         buildTree(arr, mid + 1, high, 2 * pos + 2);
         segmentTree[pos] = segmentTree[2 * pos + 1] + segmentTree[2 * pos + 2];
     }
+
 public:
-    SegmentTree(const vector<int>& arr) {
+    SegmentTree(const vector<int> &arr)
+    {
         n = arr.size();
         int treeSize = 4 * n;
         segmentTree.resize(treeSize);
         buildTree(arr, 0, n - 1, 0);
     }
-    void update(int index, int newValue) {
+    void update(int index, int newValue)
+    {
         updateElement(0, 0, n - 1, index, newValue);
     }
-    int querySum(int left, int right) {
+    int querySum(int left, int right)
+    {
         return query(0, 0, n - 1, left, right);
     }
+
 private:
-    void updateElement(int pos, int low, int high, int index, int newValue) {
-        if (low == high) {segmentTree[pos] = newValue;return;}
+    void updateElement(int pos, int low, int high, int index, int newValue)
+    {
+        if (low == high)
+        {
+            segmentTree[pos] = newValue;
+            return;
+        }
         int mid = (low + high) / 2;
-        if (index <= mid) updateElement(2 * pos + 1, low, mid, index, newValue);
-        else updateElement(2 * pos + 2, mid + 1, high, index, newValue);
+        if (index <= mid)
+            updateElement(2 * pos + 1, low, mid, index, newValue);
+        else
+            updateElement(2 * pos + 2, mid + 1, high, index, newValue);
         segmentTree[pos] = segmentTree[2 * pos + 1] + segmentTree[2 * pos + 2];
     }
-    int query(int pos, int low, int high, int left, int right) {
-        if (right < low || left > high) return 0;
-        if (left <= low && right >= high) return segmentTree[pos];
+    int query(int pos, int low, int high, int left, int right)
+    {
+        if (right < low || left > high)
+            return 0;
+        if (left <= low && right >= high)
+            return segmentTree[pos];
         int mid = (low + high) / 2;
         int leftSum = query(2 * pos + 1, low, mid, left, right);
         int rightSum = query(2 * pos + 2, mid + 1, high, left, right);
@@ -51,44 +72,56 @@ private:
 
 const int nn = 2e5;
 int ti = 0;
-vi val(nn), eTour, cnt(nn, 0), idx(nn);
+vi val(nn), eTour, cnt(nn, 0), pIdx(nn);
 vi edge[nn];
 
-void dfs(int cv, int pv){
+void dfs(int cv, int pv)
+{
     eTour.push_back(val[cv]);
-    idx[cv] = ti++;
-    for(int v : edge[cv]){
-        if(v == pv) continue;
+    pIdx[cv] = ti++;
+    for (int v : edge[cv])
+    {
+        if (v == pv)
+            continue;
         dfs(v, cv);
         cnt[cv] += 1 + cnt[v];
     }
 }
-signed main(){
+signed main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
-    int n, q; cin >> n >> q;
-    for(int i=0 ; i<n ; i++)
+
+    int n, q;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++)
         cin >> val[i];
-    for(int i=1 ; i<n ; i++){
-        int x, y; cin >> x >> y;
+    for (int i = 1; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
         --x, --y;
         edge[x].push_back(y);
         edge[y].push_back(x);
     }
     dfs(0, -1);
     SegmentTree st(eTour);
-    while(q--){
-        int tt, s, x; cin >> tt;
-        if(tt == 1){
+    while (q--)
+    {
+        int tt, s, x;
+        cin >> tt;
+        if (tt == 1)
+        {
             cin >> s >> x;
             s -= 1;
-            st.update(idx[s], x);
-        }else{
+            st.update(pIdx[s], x);
+        }
+        else
+        {
             cin >> s;
             s -= 1;
-            cout << st.querySum(idx[s], idx[s] + cnt[s]) << endl;
+            cout << st.querySum(pIdx[s], pIdx[s] + cnt[s]) << endl;
         }
     }
 }
--- Tree/11_pathQueries.cpp
+++ Tree/11_pathQueries.cpp
@@ -7,41 +7,62 @@ using namespace std;
 #define pii pair<int, int>
 #define vvi vector<vector<int>>
 
-class SegmentTree {
+class SegmentTree
+{
 private:
     vector<int> segmentTree;
     int n;
-    void buildTree(const vector<int>& arr, int low, int high, int pos) {
-        if (low == high) {segmentTree[pos] = arr[low];return;}
+    void buildTree(const vector<int> &arr, int low, int high, int pos)
+    {
+        if (low == high)
+        {
+            segmentTree[pos] = arr[low];
+            return;
+        }
         int mid = (low + high) / 2;
         buildTree(arr, low, mid, 2 * pos + 1);
         buildTree(arr, mid + 1, high, 2 * pos + 2);
         segmentTree[pos] = segmentTree[2 * pos + 1] + segmentTree[2 * pos + 2];
     }
+
 public:
-    SegmentTree(const vector<int>& arr) {
+    SegmentTree(const vector<int> &arr)
+    {
         n = arr.size();
         int treeSize = 4 * n;
         segmentTree.resize(treeSize);
         buildTree(arr, 0, n - 1, 0);
     }
-    void update(int index, int newValue) {
+    void update(int index, int newValue)
+    {
         updateElement(0, 0, n - 1, index, newValue);
     }
-    int querySum(int left, int right) {
+    int querySum(int left, int right)
+    {
         return query(0, 0, n - 1, left, right);
     }
+
 private:
-    void updateElement(int pos, int low, int high, int index, int newValue) {
-        if (low == high) {segmentTree[pos] = newValue;return;}
+    void updateElement(int pos, int low, int high, int index, int newValue)
+    {
+        if (low == high)
+        {
+            segmentTree[pos] = newValue;
+            return;
+        }
         int mid = (low + high) / 2;
-        if (index <= mid) updateElement(2 * pos + 1, low, mid, index, newValue);
-        else updateElement(2 * pos + 2, mid + 1, high, index, newValue);
+        if (index <= mid)
+            updateElement(2 * pos + 1, low, mid, index, newValue);
+        else
+            updateElement(2 * pos + 2, mid + 1, high, index, newValue);
         segmentTree[pos] = segmentTree[2 * pos + 1] + segmentTree[2 * pos + 2];
     }
-    int query(int pos, int low, int high, int left, int right) {
-        if (right < low || left > high) return 0;
-        if (left <= low && right >= high) return segmentTree[pos];
+    int query(int pos, int low, int high, int left, int right)
+    {
+        if (right < low || left > high)
+            return 0;
+        if (left <= low && right >= high)
+            return segmentTree[pos];
         int mid = (low + high) / 2;
         int leftSum = query(2 * pos + 1, low, mid, left, right);
         int rightSum = query(2 * pos + 2, mid + 1, high, left, right);
@@ -49,21 +70,61 @@ private:
     }
 };
 const int nn = 2e5;
+int ti = 0;
+vi val(nn), eTour, cnt(nn, 0), pIdx(nn), nIdx(nn);
 vi edge[nn];
-signed main(){
+
+void dfs(int cv, int pv)
+{
+    eTour.push_back(val[cv]);
+    pIdx[cv] = ti++;
+    for (int v : edge[cv])
+    {
+        if (v == pv)
+            continue;
+        dfs(v, cv);
+        cnt[cv] += 1 + cnt[v];
+    }
+    eTour.push_back(-1 * val[cv]);
+    nIdx[cv] = ti++;
+}
+signed main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
-    int n, q; cin >> n >> q;
-    for(int i=0 ; i<n ; i++)
+
+    int n, q;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++)
         cin >> val[i];
-    for(int i=1 ; i<n ; i++){
-        int x, y; cin >> x >> y;
+    for (int i = 1; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
         --x, --y;
         edge[x].push_back(y);
         edge[y].push_back(x);
     }
     dfs(0, -1);
-    SegmentTree st();
+    SegmentTree st(eTour);
+
+    while (q--)
+    {
+        int tt, s, x;
+        cin >> tt;
+        if (tt == 1)
+        {
+            cin >> s >> x;
+            s -= 1;
+            st.update(pIdx[s], x);
+            st.update(nIdx[s], -1*x);
+        }
+        else
+        {
+            cin >> s;
+            s -= 1;
+            cout << st.querySum(0, pIdx[s]) << endl;
+        }
+    }
 }
